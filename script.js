d87f647dd64b76a38127c7bd7622d9bda54c5c59
MRS WORKS <w9ptzcdty8@privaterelay.appleid.com>
2026-08-16 22:46:05 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 //   npmパッケージではなくCDNのESM URLを使用しています）
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 import {
     getDatabase,
     ref,
@@ -44,6 +45,10 @@ let firebaseReady = false;
 
 try {
     firebaseApp = initializeApp(firebaseConfig);
+
+    const auth = getAuth();
+    signInAnonymously(auth);
+
     db = getDatabase(firebaseApp);
     firebaseReady = true;
 } catch (err) {
